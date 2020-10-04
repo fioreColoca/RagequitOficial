a82640632ddcbc2e3f220f4453d07e9c2e4db922
sanchez matias <matias-leandro@outlook.com>
2020-10-04 15:00:34 -0300

    se crearon los modelos Publicacion, Comentario y Categoria

---
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/Publicacion.java
@@ -0,0 +1,82 @@
+package ar.edu.unlam.tallerweb1.modelo;
+
+
+
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Publicacion {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String mensaje;
+	private Integer cantidadLikes;
+	private Integer buff; //aun nose bien como funcionara buff
+	/*private Date fechaHora;*/
+	
+	@OneToOne
+	private Categoria categoria;
+	
+	@OneToMany
+	private List <Comentario> comentarios = new LinkedList<Comentario>();
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getMensaje() {
+		return mensaje;
+	}
+
+	public void setMensaje(String mensaje) {
+		this.mensaje = mensaje;
+	}
+
+	public Integer getCantidadLikes() {
+		return cantidadLikes;
+	}
+
+	public void setCantidadLikes(Integer cantidadLikes) {
+		this.cantidadLikes = cantidadLikes;
+	}
+
+	public Integer getBuff() {
+		return buff;
+	}
+
+	public void setBuff(Integer buff) {
+		this.buff = buff;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public List<Comentario> getComentarios() {
+		return comentarios;
+	}
+
+	public void setComentarios(List<Comentario> comentarios) {
+		this.comentarios = comentarios;
+	}
+
+	
+}
