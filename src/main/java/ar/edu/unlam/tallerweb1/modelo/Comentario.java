4a84bddcdf33bf1cc5922f743f75ab204591d371
fioreColoca <fiorecoloca99@gmail.com>
2020-10-04 17:04:00 -0300

    modelo comentario duda import date

--- src/main/java/ar/edu/unlam/tallerweb1/modelo/Comentario.java
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/Comentario.java
@@ -1,4 +1,6 @@
-/*package ar.edu.unlam.tallerweb1.modelo;
+package ar.edu.unlam.tallerweb1.modelo;
+
+import java.sql.Date;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -6,56 +8,50 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-/*public class Comentario {
-	/* ES UNARIA */
-	/*@Id
+public class Comentario {
+	/* ES UNARIA  DUDA CON EL IMPORT DE DATE*/
+	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	private String mensaje;
 	private Integer cantidadLikes;
-	private Integer darLike;
+	private Date fechaHora; 
 	private Integer buff; /* nose tampoco :C */
 	
 	
-	/*public Long getId() {
+	public Long getId() {
 		return id;
 	}
-	
 	public void setId(Long id) {
 		this.id = id;
 	}
-	
 	public String getMensaje() {
 		return mensaje;
 	}
-	
 	public void setMensaje(String mensaje) {
 		this.mensaje = mensaje;
 	}
-	
 	public Integer getCantidadLikes() {
 		return cantidadLikes;
 	}
-	
 	public void setCantidadLikes(Integer cantidadLikes) {
 		this.cantidadLikes = cantidadLikes;
 	}
-	
-	public Integer getDarLike() {
-		return darLike;
+	public Date getFechaHora() {
+		return fechaHora;
 	}
-	
-	public void setDarLike(Integer darLike) {
-		this.darLike = darLike;
+	public void setFechaHora(Date fechaHora) {
+		this.fechaHora = fechaHora;
 	}
-	
 	public Integer getBuff() {
 		return buff;
 	}
-	
 	public void setBuff(Integer buff) {
 		this.buff = buff;
-	} 
+	}
 	
-}*/
+	
+	
+	
+}
