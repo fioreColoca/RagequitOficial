d7ec56222582a790fac88715fc8b2b510e469563
fioreColoca <fiorecoloca99@gmail.com>
2020-10-04 16:51:26 -0300

    Cambios en comentario modelo

--- src/main/java/ar/edu/unlam/tallerweb1/modelo/Comentario.java
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/Comentario.java
@@ -1,5 +1,61 @@
-package ar.edu.unlam.tallerweb1.modelo;
+/*package ar.edu.unlam.tallerweb1.modelo;
 
-public class Comentario {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-}
+@Entity
+/*public class Comentario {
+	/* ES UNARIA */
+	/*@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String mensaje;
+	private Integer cantidadLikes;
+	private Integer darLike;
+	private Integer buff; /* nose tampoco :C */
+	
+	
+	/*public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getMensaje() {
+		return mensaje;
+	}
+	
+	public void setMensaje(String mensaje) {
+		this.mensaje = mensaje;
+	}
+	
+	public Integer getCantidadLikes() {
+		return cantidadLikes;
+	}
+	
+	public void setCantidadLikes(Integer cantidadLikes) {
+		this.cantidadLikes = cantidadLikes;
+	}
+	
+	public Integer getDarLike() {
+		return darLike;
+	}
+	
+	public void setDarLike(Integer darLike) {
+		this.darLike = darLike;
+	}
+	
+	public Integer getBuff() {
+		return buff;
+	}
+	
+	public void setBuff(Integer buff) {
+		this.buff = buff;
+	} 
+	
+}*/
