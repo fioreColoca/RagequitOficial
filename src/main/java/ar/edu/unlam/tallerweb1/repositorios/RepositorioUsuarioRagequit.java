2debf343792cc834bb2e4e26580341945f2bfe93
SantiFagliano <fagliano.santi@gmail.com>
2020-10-05 18:28:40 -0300

    Corregi y puse Repositorios

--- src/main/java/ar/edu/unlam/tallerweb1/repositorios/RepositorioUsuarioRagequit.java
+++ src/main/java/ar/edu/unlam/tallerweb1/repositorios/RepositorioUsuarioRagequit.java
@@ -2,6 +2,6 @@ package ar.edu.unlam.tallerweb1.repositorios;
 
 import ar.edu.unlam.tallerweb1.modelo.UsuarioRagequit;
 
-public interface ServicioUsuarioRagequit {
+public interface RepositorioUsuarioRagequit {
 	void guardarUsuario(UsuarioRagequit usuarioRagequit);
 }
--- src/main/java/ar/edu/unlam/tallerweb1/repositorios/RepositorioUsuarioRagequitImpl.java
+++ src/main/java/ar/edu/unlam/tallerweb1/repositorios/RepositorioUsuarioRagequitImpl.java
@@ -3,10 +3,12 @@ package ar.edu.unlam.tallerweb1.repositorios;
 import javax.inject.Inject;
 
 import org.hibernate.SessionFactory;
+import org.springframework.stereotype.Service;
 
 import ar.edu.unlam.tallerweb1.modelo.UsuarioRagequit;
 
-public class ServicioUsuarioRagequitImpl implements ServicioUsuarioRagequit {
+@Service
+public class RepositorioUsuarioRagequitImpl implements RepositorioUsuarioRagequit {
 
 	@Inject
 	private SessionFactory sessionFactory;
