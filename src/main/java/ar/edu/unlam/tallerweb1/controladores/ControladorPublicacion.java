9bf9a6620f1ce9e4fe1b0c57a3285550c975c9db
sanchez matias <matias-leandro@outlook.com>
2020-10-05 14:31:37 -0300

    Se agrego al ControladorPublicacion los ModelAndView registrarPublicacion y confirmacionPublicacion

--- src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorPublicacion.java
+++ src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorPublicacion.java
@@ -1,5 +1,41 @@
 package ar.edu.unlam.tallerweb1.controladores;
 
-public class ControladorPublicacion {
+import javax.inject.Inject;
 
-}
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import ar.edu.unlam.tallerweb1.modelo.Publicacion;
+@Controller
+public class ControladorPublicacion {
+	
+	@Inject
+	private ServicioPublicacion servicioPublicacion;
+
+	@RequestMapping(path="/registrarPublicacion")
+	public ModelAndView registrarPublicacion() {
+		return new ModelAndView("publicacionRegistrar");
+	}
+	
+	@RequestMapping(path= "/confirmacionRegistroPublicacion", method = RequestMethod.GET)
+	public ModelAndView confirmacionPublicacion(
+			/*@RequestParam(value = "nombreCampeon", required = false) String nombreCampeon,
+			@RequestParam(value = "rolCampeon", required = false) String rolCampeon*/
+			) {
+		Publicacion publicacion = new Publicacion();
+		ModelMap modelo = new ModelMap();
+		
+		
+		Long idpublicacion = servicioPublicacion.guardarPublicacion(publicacion);
+		
+		modelo.put("idpublicacion", idpublicacion);
+		
+		
+		
+		return new ModelAndView("publicacionRegistradaConfirmacion", modelo);
+	}
+}	
