7fda31e73473e5c96c6525d14cb4fdde87f9f1f2
sanchez matias <matias-leandro@outlook.com>
2020-10-05 14:19:48 -0300

    Se cambio la categoria a @ManyToOne

--- src/main/java/ar/edu/unlam/tallerweb1/modelo/Publicacion.java
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/Publicacion.java
@@ -1,74 +1,72 @@
-//package ar.edu.unlam.tallerweb1.modelo;
-//
-//
-//import java.util.Date;
-//import java.util.LinkedList;
-//import java.util.List;
-//
-//import javax.persistence.Entity;
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.GenerationType;
-//import javax.persistence.Id;
-//import javax.persistence.OneToMany;
-//import javax.persistence.OneToOne;
-//
-//@Entity
-//public class Publicacion {
-//	
-//	@Id
-//	@GeneratedValue(strategy = GenerationType.IDENTITY)
-//	private Long id;
-//	
-//	private String mensaje;
-//	private Integer contadorLikes;
-//	private Date fechaHora;
-//	
-//	@OneToOne
-//	private Categoria categoria;
-//	
-//	@OneToMany
-//	private List <Comentario> comentarios = new LinkedList<Comentario>();
-//
-//	public Long getId() {
-//		return id;
-//	}
-//
-//	public void setId(Long id) {
-//		this.id = id;
-//	}
-//
-//	public String getMensaje() {
-//		return mensaje;
-//	}
-//
-//	public void setMensaje(String mensaje) {
-//		this.mensaje = mensaje;
-//	}
-//
-//	public Integer getCantidadLikes() {
-//		return contadorLikes;
-//	}
-//
-//	public void setCantidadLikes(Integer cantidadLikes) {
-//		this.contadorLikes = cantidadLikes;
-//	}
-//
-//
-//	public Categoria getCategoria() {
-//		return categoria;
-//	}
-//
-//	public void setCategoria(Categoria categoria) {
-//		this.categoria = categoria;
-//	}
-//
-//	public List<Comentario> getComentarios() {
-//		return comentarios;
-//	}
-//
-//	public void setComentarios(List<Comentario> comentarios) {
-//		this.comentarios = comentarios;
-//	}
-//
-//	
-//}
+package ar.edu.unlam.tallerweb1.modelo;
+
+
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Publicacion {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String mensaje;
+	private Integer contadorLikes;
+	private Date fechaHora;
+	
+	@ManyToOne
+	private Categoria categoria;
+	
+	@OneToMany
+	private List <Comentario> comentarios = new LinkedList<Comentario>();
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getMensaje() {
+		return mensaje;
+	}
+
+	public void setMensaje(String mensaje) {
+		this.mensaje = mensaje;
+	}
+
+	public Integer getCantidadLikes() {
+		return contadorLikes;
+	}
+
+	public void setCantidadLikes(Integer cantidadLikes) {
+		this.contadorLikes = cantidadLikes;
+	}
+
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public List<Comentario> getComentarios() {
+		return comentarios;
+	}
+
+	public void setComentarios(List<Comentario> comentarios) {
+		this.comentarios = comentarios;
+	}
+}
