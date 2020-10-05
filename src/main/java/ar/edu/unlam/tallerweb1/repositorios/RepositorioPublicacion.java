31ef20db783439856e1851faa39cff79baccbf98
sanchez matias <matias-leandro@outlook.com>
2020-10-05 14:47:15 -0300

    Se creo RepositorioPublicacion y RepositorioPublicacionImpl

---
+++ src/main/java/ar/edu/unlam/tallerweb1/repositorios/RepositorioPublicacion.java
@@ -0,0 +1,9 @@
+package ar.edu.unlam.tallerweb1.repositorios;
+
+import ar.edu.unlam.tallerweb1.modelo.Publicacion;
+
+public interface RepositorioPublicacion {
+
+	Long guardarPublicacion(Publicacion publicacion);
+
+}
