e15c0299b7289fb99d99741d80c94818747e772d
sanchez matias <matias-leandro@outlook.com>
2020-10-05 15:09:53 -0300

    Se agrego un RequestParam al ControladorPublicacion. Metodo confirmacionPublicacion

--- src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorPublicacion.java
+++ src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorPublicacion.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import ar.edu.unlam.tallerweb1.modelo.Publicacion;
+import ar.edu.unlam.tallerweb1.servicios.ServicioPublicacion;
 @Controller
 public class ControladorPublicacion {
 	
@@ -23,18 +24,20 @@ public class ControladorPublicacion {
 	
 	@RequestMapping(path= "/confirmacionRegistroPublicacion", method = RequestMethod.GET)
 	public ModelAndView confirmacionPublicacion(
-			/*@RequestParam(value = "nombreCampeon", required = false) String nombreCampeon,
-			@RequestParam(value = "rolCampeon", required = false) String rolCampeon*/
+			@RequestParam(value = "mensajePublicacion", required = false) String mensajePublicacion,
+			@RequestParam(value = "categoriaPublicacion", required = false) String categoriaPublicacion
 			) {
 		Publicacion publicacion = new Publicacion();
 		ModelMap modelo = new ModelMap();
 		
+		publicacion.setMensaje(mensajePublicacion);
 		
 		Long idpublicacion = servicioPublicacion.guardarPublicacion(publicacion);
 		
 		modelo.put("idpublicacion", idpublicacion);
+		modelo.put("publicacion",publicacion);
 		
-		
+		modelo.put("categoria", categoriaPublicacion);
 		
 		return new ModelAndView("publicacionRegistradaConfirmacion", modelo);
 	}
