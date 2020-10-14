01d87fe6037a0d59971fa8d33ab13585930108cb
alopezducas <63357670+alopezducas@users.noreply.github.com>
2020-10-14 17:16:07 -0300

    se mejoró elcontrolador de categoria

--- src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorCategoria.java
+++ src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorCategoria.java
@@ -5,9 +5,11 @@ import javax.inject.Inject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import ar.edu.unlam.tallerweb1.modelo.Categoria;
+import ar.edu.unlam.tallerweb1.modelo.CategoriaTipo;
 import ar.edu.unlam.tallerweb1.servicios.ServicioCategoria;
 
 @Controller
@@ -18,10 +20,28 @@ public class ControladorCategoria {
 	
 	@RequestMapping("/categoria")
 	public ModelAndView irACategoria(){
+		return new ModelAndView("categoria");
+	}
+	
+	@RequestMapping("/confirmacionCategoria")
+	public ModelAndView categoriaExitosa(
+			@RequestParam(value = "categoria", required = false) String tipoCategoria,
+			@RequestParam(value = "crearCategoria", required = false) String nombreCategoria) {
 		ModelMap modelo = new ModelMap();
 		Categoria categoria = new Categoria();
+
+		if(tipoCategoria.equals("Juegos")) {
+			categoria.setTipoCategoria(CategoriaTipo.JUEGOS);
+		} else {
+			categoria.setTipoCategoria(CategoriaTipo.VARIOS);
+		}
+		
+		categoria.setNombre(nombreCategoria);
+		
+		servicioCategoria.crearCategoria(categoria);
+		
 		modelo.put("categoria", categoria);
-		return new ModelAndView("categoria", modelo);
+		return new ModelAndView("confirmacionCategoria",modelo);
 	}
 	
 
