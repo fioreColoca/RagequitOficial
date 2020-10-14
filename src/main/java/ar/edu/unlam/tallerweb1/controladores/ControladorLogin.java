31c422ff36d770f02edc8160522972916640fdc6
SantiFagliano <fagliano.santi@gmail.com>
2020-10-14 13:23:28 -0300

    realizamos el registro del usuario

--- src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorLogin.java
+++ src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorLogin.java
@@ -8,6 +8,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -15,40 +16,50 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 public class ControladorLogin {
 
-	// La anotacion @Autowired indica a Spring que se debe utilizar el contructor como mecanismo de inyección de dependencias,
-	// es decir, qeue lo parametros del mismo deben ser un bean de spring y el framewrok automaticamente pasa como parametro
-	// el bean correspondiente, en este caso, un objeto de una clase que implemente la interface ServicioLogin,
-	// dicha clase debe estar anotada como @Service o @Repository y debe estar en un paquete de los indicados en
+	// La anotacion @Autowired indica a Spring que se debe utilizar el contructor
+	// como mecanismo de inyección de dependencias,
+	// es decir, qeue lo parametros del mismo deben ser un bean de spring y el
+	// framewrok automaticamente pasa como parametro
+	// el bean correspondiente, en este caso, un objeto de una clase que implemente
+	// la interface ServicioLogin,
+	// dicha clase debe estar anotada como @Service o @Repository y debe estar en un
+	// paquete de los indicados en
 	// applicationContext.xml
 	private ServicioLogin servicioLogin;
 
 	@Autowired
-	public ControladorLogin(ServicioLogin servicioLogin){
+	public ControladorLogin(ServicioLogin servicioLogin) {
 		this.servicioLogin = servicioLogin;
 	}
 
-	// Este metodo escucha la URL localhost:8080/NOMBRE_APP/login si la misma es invocada por metodo http GET
+	// Este metodo escucha la URL localhost:8080/NOMBRE_APP/login si la misma es
+	// invocada por metodo http GET
 	@RequestMapping("/login")
 	public ModelAndView irALogin() {
 
 		ModelMap modelo = new ModelMap();
-		// Se agrega al modelo un objeto del tipo Usuario con key 'usuario' para que el mismo sea asociado
+		// Se agrega al modelo un objeto del tipo Usuario con key 'usuario' para que el
+		// mismo sea asociado
 		// al model attribute del form que esta definido en la vista 'login'
 		Usuario usuario = new Usuario();
 		modelo.put("usuario", usuario);
-		// Se va a la vista login (el nombre completo de la lista se resuelve utilizando el view resolver definido en el archivo spring-servlet.xml)
-		// y se envian los datos a la misma  dentro del modelo
+		// Se va a la vista login (el nombre completo de la lista se resuelve utilizando
+		// el view resolver definido en el archivo spring-servlet.xml)
+		// y se envian los datos a la misma dentro del modelo
 		return new ModelAndView("login", modelo);
 	}
 
-	// Este metodo escucha la URL validar-login siempre y cuando se invoque con metodo http POST
-	// El método recibe un objeto Usuario el que tiene los datos ingresados en el form correspondiente y se corresponde con el modelAttribute definido en el
+	// Este metodo escucha la URL validar-login siempre y cuando se invoque con
+	// metodo http POST
+	// El método recibe un objeto Usuario el que tiene los datos ingresados en el
+	// form correspondiente y se corresponde con el modelAttribute definido en el
 	// tag form:form
 	@RequestMapping(path = "/validar-login", method = RequestMethod.POST)
 	public ModelAndView validarLogin(@ModelAttribute("usuario") Usuario usuario, HttpServletRequest request) {
 		ModelMap model = new ModelMap();
 
-		// invoca el metodo consultarUsuario del servicio y hace un redirect a la URL /home, esto es, en lugar de enviar a una vista
+		// invoca el metodo consultarUsuario del servicio y hace un redirect a la URL
+		// /home, esto es, en lugar de enviar a una vista
 		// hace una llamada a otro action a través de la URL correspondiente a ésta
 		Usuario usuarioBuscado = servicioLogin.consultarUsuario(usuario);
 		if (usuarioBuscado != null) {
@@ -62,21 +73,41 @@ public class ControladorLogin {
 	}
 
 	// Escucha la URL /home por GET, y redirige a una vista.
-	/*@RequestMapping(path = "/home", method = RequestMethod.GET)
-	public ModelAndView irAHome() {
-		ModelMap modelo = new ModelMap();
-		modelo.put("Titulo","Home");
-		return new ModelAndView("home",modelo);
-	}*/
+	/*
+	 * @RequestMapping(path = "/home", method = RequestMethod.GET) public
+	 * ModelAndView irAHome() { ModelMap modelo = new ModelMap();
+	 * modelo.put("Titulo","Home"); return new ModelAndView("home",modelo); }
+	 */
 
-	// Escucha la url /, y redirige a la URL /login, es lo mismo que si se invoca la url /login directamente.
+	// Escucha la url /, y redirige a la URL /login, es lo mismo que si se invoca la
+	// url /login directamente.
 	@RequestMapping(path = "/", method = RequestMethod.GET)
 	public ModelAndView inicio() {
 		return new ModelAndView("redirect:/login");
 	}
+
 	@RequestMapping(path = "registrar")
 	public ModelAndView registrar() {
 		ModelMap modelo = new ModelMap();
 		return new ModelAndView("crearUsuario",modelo);
+	}
+
+	@RequestMapping(path = "/registrando")
+	public ModelAndView registrarUsuario(@RequestParam(value = "nombre", required = false) String nombre,
+			@RequestParam(value = "apellido", required = false) String apellido,
+			@RequestParam(value = "email", required = false) String email,
+			@RequestParam(value = "telefono", required = false) String telefono,
+			@RequestParam(value = "usuario", required = false) String usuario,
+			@RequestParam(value = "contrasenia", required = false) String contrasenia) {
+		ModelMap modelo = new ModelMap();
+		Usuario usuariov1= new Usuario();
+		usuariov1.setApellido(apellido);
+		usuariov1.setEmail(email);
+		usuariov1.setNombre(nombre);
+		usuariov1.setNombreUsuario(usuario);
+		usuariov1.setPassword(contrasenia);
+		usuariov1.setRol("Admin");
+		servicioLogin.registrarUsuario(usuariov1);
+		return new ModelAndView("redirect:/login",modelo);
 	}
 }
