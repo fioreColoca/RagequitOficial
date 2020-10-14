1b87dea24a619a4f307a206229b93fcd106b3b95
SantiFagliano <fagliano.santi@gmail.com>
2020-10-14 11:13:41 -0300

    modificamos el usuario agregandole atributos

--- src/main/java/ar/edu/unlam/tallerweb1/modelo/Usuario.java
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/Usuario.java
@@ -1,5 +1,7 @@
 package ar.edu.unlam.tallerweb1.modelo;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -11,31 +13,128 @@ import javax.persistence.Id;
 @Entity
 public class Usuario {
 
-	// La anotacion id indica que este atributo es el utilizado como clave primaria de la entity, se indica que el valor es autogenerado.
+	// La anotacion id indica que este atributo es el utilizado como clave primaria
+	// de la entity, se indica que el valor es autogenerado.
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	// para el resto de los atributo no se usan anotaciones entonces se usa el default de hibernate: la columna se llama igual que
-	// el atributo, la misma admite nulos, y el tipo de dato se deduce del tipo de dato de java.
+	// para el resto de los atributo no se usan anotaciones entonces se usa el
+	// default de hibernate: la columna se llama igual que
+	// el atributo, la misma admite nulos, y el tipo de dato se deduce del tipo de
+	// dato de java.
 	private String email;
 	private String password;
 	private String rol;
-	
+	private String nombreUsuario;
+	private Date fechaCreacion;
+	private Date fechaNacimiento;
+	private Integer nivel;
+	private String nombre;
+	private String apellido;
+	private Integer contadorSeguidos;
+	private Integer contadorSuscriptores;
+	private Integer contadorSeguidores;
+
+	// Array de seguidores
+	// @ManyToMany
+	// private List<Usuario> arraySeguidores = new LinkedList<Usuario>();
+
+	// Array de Suscriptores
+	// @ManyToMany
+	// private List<Usuario> arraySuscriptores = new LinkedList<Usuario>();
+
+	public String getNombreUsuario() {
+		return nombreUsuario;
+	}
+
+	public void setNombreUsuario(String nombreUsuario) {
+		this.nombreUsuario = nombreUsuario;
+	}
+
+	public Date getFechaCreacion() {
+		return fechaCreacion;
+	}
+
+	public void setFechaCreacion(Date fechaCreacion) {
+		this.fechaCreacion = fechaCreacion;
+	}
+
+	public Date getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(Date fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	public Integer getNivel() {
+		return nivel;
+	}
+
+	public void setNivel(Integer nivel) {
+		this.nivel = nivel;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public Integer getContadorSeguidos() {
+		return contadorSeguidos;
+	}
+
+	public void setContadorSeguidos(Integer contadorSeguidos) {
+		this.contadorSeguidos = contadorSeguidos;
+	}
+
+	public Integer getContadorSuscriptores() {
+		return contadorSuscriptores;
+	}
+
+	public void setContadorSuscriptores(Integer contadorSuscriptores) {
+		this.contadorSuscriptores = contadorSuscriptores;
+	}
+
+	public Integer getContadorSeguidores() {
+		return contadorSeguidores;
+	}
+
+	public void setContadorSeguidores(Integer contadorSeguidores) {
+		this.contadorSeguidores = contadorSeguidores;
+	}
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
