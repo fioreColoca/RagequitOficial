688bbfebfcc6bb3cc4b32ccac8bccaab1d1d2ecd
alopezducas <63357670+alopezducas@users.noreply.github.com>
2020-10-14 22:27:30 -0300

    se agrega la lista de categorias hechas

--- src/main/java/ar/edu/unlam/tallerweb1/repositorios/RepositorioCategoria.java
+++ src/main/java/ar/edu/unlam/tallerweb1/repositorios/RepositorioCategoria.java
@@ -1,5 +1,7 @@
 package ar.edu.unlam.tallerweb1.repositorios;
 
+import java.util.List;
+
 import ar.edu.unlam.tallerweb1.modelo.Categoria;
 
 public interface RepositorioCategoria {
@@ -7,5 +9,7 @@ public interface RepositorioCategoria {
 	void crearCategoria(Categoria categoria);
 	
 	Categoria mostrarCategoriaPorId(Long id);
+	
+	List<Categoria> mostrarCategorias();
 
 }
