fcd7eac7d3f13f30aff089143fe08f79a3617960
sanchez matias <matias-leandro@outlook.com>
2020-10-15 19:24:42 -0300

    se comento cambio el nombre del controladorIndex a controladorPublicacion

--- src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorPublicacion.java
+++ src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorPublicacion.java
@@ -7,78 +7,66 @@ import javax.inject.Inject;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+
 import ar.edu.unlam.tallerweb1.modelo.Publicacion;
 import ar.edu.unlam.tallerweb1.servicios.ServicioPublicacion;
 
 @Controller
-public class ControladorIndex {
-	
+public class ControladorPublicacion {
+
 	@Inject
 	private ServicioPublicacion servicioPublicacion;
-	
-	@RequestMapping(path="home")
+
+	@RequestMapping(path = "home")
 	public ModelAndView irAlHome() {
 		ModelMap modelo = new ModelMap();
+		Publicacion publicacion = new Publicacion();
+		List<Publicacion> publicaciones = servicioPublicacion.buscarPublicaciones();
 		modelo.put("title", "Inicio");
+		modelo.put("publicaciones", publicaciones);
+		modelo.put("publicacion", publicacion);
 		return new ModelAndView("home", modelo);
 	}
-	
-	@RequestMapping(path= "/guardarPublicacion", method = RequestMethod.GET)
-	public ModelAndView guardarPublicacion(
-			@RequestParam(value = "mensajePublicacion", required = false) String mensajePublicacion,
-			@RequestParam(value = "categoriaPublicacion", required = false) String categoriaPublicacion
-			) throws Exception {
+
+	@RequestMapping(path = "/guardarPublicacion", method = RequestMethod.POST)
+	public ModelAndView guardarPublicacion(@ModelAttribute("publicacion") Publicacion publicacion) {
 		Date fecha = new Date();
-		Publicacion publicacion = new Publicacion();
 		ModelMap modelo = new ModelMap();
-		
-		publicacion.setMensaje(mensajePublicacion);
 		publicacion.setFechaHora(fecha);
-		publicacion.setCategoria(categoriaPublicacion);
-		
-		
+
 		try {
 			servicioPublicacion.guardarPublicacion(publicacion);
-		}catch(Exception e){
+		} catch (Exception e) {
 			String error = e.getMessage();
 			modelo.put("errorCategoriaVacia", error);
+			return new ModelAndView("home", modelo);
 		}
 		
-		return new ModelAndView("redirect:/mostrarListaPublicacion", modelo);
+		return new ModelAndView("redirect:/home");
 	}
-	
-/*	@RequestMapping(path="/mostrarListaPublicacion")
-	public ModelAndView mostrarPublicacion() {
-		ModelMap modelo = new ModelMap();
-		
-		
-		List<Publicacion> publicaciones = servicioPublicacion.buscarPublicaciones();
-		
-		return new ModelAndView("redirect:home",modelo);
-	}
-	
-	@RequestMapping(path= "/borrarPublicacion", method = RequestMethod.GET)
+
+	@RequestMapping(path = "/borrarPublicacion", method = RequestMethod.GET)
 	public ModelAndView borrarPublicacion(
-			@RequestParam(value = "botonBorrar", required = false) Long id
-			) throws Exception {
+			@RequestParam(value = "botonBorrar", required = false) Long id){
 		servicioPublicacion.borrarPublicacion(id);
-		
-		return new ModelAndView("redirect:/mostrarListaPublicacion");
+
+		return new ModelAndView("redirect:/home");
 	}
-	*/
-	
-	@RequestMapping(path= "/filtrarCategoria", method = RequestMethod.GET)
-	public ModelAndView borrarPublicacion(
-			@RequestParam(value = "filtarPublicacionCategoria", required = false) String filtrarPublicacionCategoria
-			) throws Exception {
-		
+
+	/*@RequestMapping(path = "/filtrarCategoria", method = RequestMethod.GET)
+	public ModelAndView filtrarPublicacion(
+			@RequestParam(value = "filtarPublicacionCategoria", required = false) String filtrarPublicacionCategoria)
+			throws Exception {
+		ModelMap modelo = new ModelMap();
 		List<Publicacion> publicaciones = servicioPublicacion.buscarPublicacionesPorCategoria(filtrarPublicacionCategoria);
-		
-		return new ModelAndView("redirect:home");
-	}
+		modelo.put("publicaciones", publicaciones);
+
+		return new ModelAndView("home", modelo);
+	}*/
 }
