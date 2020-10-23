56574d219feb16bb5ee2f3f1835e4d173cb2991a
sanchez matias <matias-leandro@outlook.com>
2020-10-23 11:50:15 -0300

    se arreglo script jquery

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
-	$(document).on("click", ".botonBorrar", function () {
+	$('.botonBorrar').click(function () {
     	var miElementoId = $(this).data('id');
     	$(".modal-footer #botonBorrar").val( miElementoId );
 	});
 
-	$(document).on("click", ".responderComentario", function () {
+	$('.responderComentario').click(function () {
     	var miComentarioId = $(this).data('id');
     	$(".responderComent #responderComentario").val( miComentarioId );
 	});
