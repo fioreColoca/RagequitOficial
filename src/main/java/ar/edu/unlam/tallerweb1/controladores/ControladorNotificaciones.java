da066deeefb571fd4742945c0ac2edb364abc2e2
sanchez matias <matias-leandro@outlook.com>
2020-12-02 17:39:46 -0300

    ahora se muestran las notificaciones de dar like a una publicacion

--- src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorNotificaciones.java
+++ src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorNotificaciones.java
@@ -1,5 +1,8 @@
 package ar.edu.unlam.tallerweb1.controladores;
 
+import java.util.List;
+
+import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
@@ -7,10 +10,14 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import ar.edu.unlam.tallerweb1.modelo.Notificacion;
 import ar.edu.unlam.tallerweb1.modelo.Usuario;
+import ar.edu.unlam.tallerweb1.servicios.ServicioNotificacion;
 
 @Controller
 public class ControladorNotificaciones {
+	@Inject
+	ServicioNotificacion servicioNotificacion;
 	
 	@RequestMapping(path = "/notificaciones")
 	public ModelAndView irAlHome(HttpServletRequest request) {
@@ -19,9 +26,12 @@ public class ControladorNotificaciones {
 		Usuario usuarioLogeado = request.getSession().getAttribute("USUARIO") != null
 				? (Usuario) request.getSession().getAttribute("USUARIO")
 				: null;
-				
-		modelo.put("title", "RageQuit | Notificaciones");		
+		List<Notificacion> notificacionesUsuario= servicioNotificacion.obtenerListaDeNotificacionesDelUsuario(usuarioLogeado);
+		
+		modelo.put("title", "RageQuit | Notificaciones");
+		modelo.put("notificaciones", notificacionesUsuario);
 		modelo.put("usuarioLogeado", usuarioLogeado);
+		
 		return new ModelAndView("notificaciones", modelo);
 	}
 }
