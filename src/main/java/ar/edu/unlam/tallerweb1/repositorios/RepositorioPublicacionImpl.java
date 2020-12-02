9401280c31dbe4e603fac3cc64e69f4c237a18ae
SantiFagliano <fagliano.santi@gmail.com>
2020-12-02 16:37:01 -0300

    mostramos las publicaciones del usuario en su perfil

--- src/main/java/ar/edu/unlam/tallerweb1/repositorios/RepositorioPublicacionImpl.java
+++ src/main/java/ar/edu/unlam/tallerweb1/repositorios/RepositorioPublicacionImpl.java
@@ -11,37 +11,34 @@ import org.springframework.stereotype.Repository;
 
 import ar.edu.unlam.tallerweb1.modelo.Categoria;
 import ar.edu.unlam.tallerweb1.modelo.Publicacion;
+import ar.edu.unlam.tallerweb1.modelo.Usuario;
 
 @Repository
 public class RepositorioPublicacionImpl implements RepositorioPublicacion {
 
 	@Inject
 	private SessionFactory sessionFactory;
-	
+
 	@Override
 	public Long guardarPublicacion(Publicacion publicacion) {
 		Session session = sessionFactory.getCurrentSession();
-		return (Long)session.save(publicacion);
+		return (Long) session.save(publicacion);
 	}
 
 	@Override
 	public List<Publicacion> buscarPublicacionesPorCategoria(Categoria categoria) {
-		return sessionFactory.getCurrentSession()
-				.createCriteria(Publicacion.class)
-				.add(Restrictions.eq("categoria", categoria))
-				.list();
+		return sessionFactory.getCurrentSession().createCriteria(Publicacion.class)
+				.add(Restrictions.eq("categoria", categoria)).list();
 	}
 
 	@Override
 	public List<Publicacion> buscarPublicaciones() {
-		return sessionFactory.getCurrentSession()
-				.createCriteria(Publicacion.class)
-				.list();
+		return sessionFactory.getCurrentSession().createCriteria(Publicacion.class).list();
 	}
-	
+
 	@Override
 	public Publicacion obtenerPublicacionPorId(Long id) {
-		
+
 		return sessionFactory.getCurrentSession().get(Publicacion.class, id);
 	}
 
@@ -49,11 +46,13 @@ public class RepositorioPublicacionImpl implements RepositorioPublicacion {
 	public void borrarPublicacion(Long id) {
 		Publicacion publicacion = obtenerPublicacionPorId(id);
 		sessionFactory.getCurrentSession().delete(publicacion);
-		
+
 	}
 
-	
-	
-	
+	@Override
+	public List<Publicacion> devolverUnaListaDePublicacionesHechasPorUnUsuario(Usuario usuario) {
+		return sessionFactory.getCurrentSession().createCriteria(Publicacion.class)
+				.add(Restrictions.eq("usuario", usuario)).list();
+	}
 
 }
