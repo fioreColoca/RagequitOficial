4831fd4c185f96d5c8e289d6fb224fa845ddc053
sanchez matias <matias-leandro@outlook.com>
2020-12-02 16:56:11 -0300

    se agrego usuarioLogeado

--- src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorNotificaciones.java
+++ src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorNotificaciones.java
@@ -1,17 +1,27 @@
 package ar.edu.unlam.tallerweb1.controladores;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import ar.edu.unlam.tallerweb1.modelo.Usuario;
+
 @Controller
 public class ControladorNotificaciones {
 	
 	@RequestMapping(path = "/notificaciones")
-	public ModelAndView irAlHome() {
+	public ModelAndView irAlHome(HttpServletRequest request) {
 		ModelMap modelo = new ModelMap();
 		
+		Usuario usuarioLogeado = request.getSession().getAttribute("USUARIO") != null
+				? (Usuario) request.getSession().getAttribute("USUARIO")
+				: null;
+				
+		modelo.put("title", "RageQuit | Notificaciones");		
+		modelo.put("usuarioLogeado", usuarioLogeado);
 		return new ModelAndView("notificaciones", modelo);
 	}
 }
