84a169cb5f6210b9ffddd923d39021c643c6cb54
SantiFagliano <fagliano.santi@gmail.com>
2020-12-15 23:42:11 -0300

    agregamos la base de las criticas hacia las categorias

---
+++ src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorCriticaCategoria.java
@@ -0,0 +1,15 @@
+//package ar.edu.unlam.tallerweb1.controladores;
+//
+//import javax.inject.Inject;
+//
+//import org.springframework.stereotype.Controller;
+//
+//import ar.edu.unlam.tallerweb1.servicios.ServicioCriticaCategoria;
+//
+//@Controller
+//public class ControladorCriticaCategoria {
+//	@Inject
+//	private ServicioCriticaCategoria servicioCriticaCategoria;
+//	
+//	
+//}
